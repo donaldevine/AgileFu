cd4473329ca98f46bacf5c5b412afe6d9220aa7b
Donal Devine <donaldevine@gmail.com>
2013-12-13 11:02:13 +0000

    edited product backlog creation

--- app/models/product_backlog.rb
+++ app/models/product_backlog.rb
@@ -1,3 +1,5 @@
 class ProductBacklog < ActiveRecord::Base
   attr_accessible :description, :iteration, :priority, :reason, :title
+
+  belongs_to :project
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,5 @@
 class Project < ActiveRecord::Base
   attr_accessible :description, :name, :target_completion_date, :target_start_date
+
+  has_many :product_backlogs
 end
