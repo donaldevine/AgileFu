caed1b47ee998e60d5e218f1bacca1bb7f7df1ea
Donal Devine <donaldevine@gmail.com>
2013-12-13 09:46:15 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Agilefu::Application.config.secret_token = '4b4a5f7769544402b8009e0d8d97cb6c8d7491e56a90dc5411746b281074684c90f4969a8286c1cef0b75f7a186c50d826361410db2651e5fc3b96049034ec17'
