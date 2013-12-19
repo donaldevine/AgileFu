0183d297873dc57dd8838e82d43dc68f15ea5618
Donal Devine <donaldevine@gmail.com>
2013-12-19 17:28:58 +0000

    jquery datepickers

--- app/assets/javascripts/date_fu.js
+++ app/assets/javascripts/date_fu.js
@@ -1,6 +1,9 @@
 /**
- * Javascript to attach date picker to certain fields
+ * Script to attach date picker to certain fields
  */
 $(function(){
     $('#project_target_start_date').datepicker();
+    $('#project_target_completion_date').datepicker();
+    $('#sprint_start_date').datepicker();
+    $('#sprint_end_date').datepicker();
 });
