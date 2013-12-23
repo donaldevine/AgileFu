8ae69d76fc9b80dbf9cd34c2e052947f2aceaeb5
Donal Devine <donaldevine@gmail.com>
2013-12-23 07:41:23 +0000

    bug fixes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131223024444) do
+ActiveRecord::Schema.define(:version => 20131223062621) do
 
   create_table "product_backlogs", :force => true do |t|
     t.string   "title"
@@ -91,8 +91,8 @@ ActiveRecord::Schema.define(:version => 20131223024444) do
   add_index "tasks", ["sprint_id"], :name => "index_tasks_on_sprint_id"
 
   create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "email",                  :default => "",    :null => false
+    t.string   "encrypted_password",     :default => "",    :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -101,8 +101,9 @@ ActiveRecord::Schema.define(:version => 20131223024444) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
+    t.datetime "created_at",                                :null => false
+    t.datetime "updated_at",                                :null => false
+    t.boolean  "admin",                  :default => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
